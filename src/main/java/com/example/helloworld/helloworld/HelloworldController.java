f9e771b7bd3267b14b999ffaf4d6162724490fb8
tmpuser-at-498588106976 <>
2022-04-21 08:29:38 +0900

    JUnit5によるコントローラー、サービスのテストを追加

--- src/main/java/com/example/helloworld/helloworld/HelloworldController.java
+++ src/main/java/com/example/helloworld/helloworld/HelloworldController.java
@@ -1,13 +1,22 @@
 package com.example.helloworld.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloworldController {
 
+    private HelloworldService helloworldService;
+
+    public HelloworldController(@Autowired HelloworldService helloworldService) {
+        this.helloworldService = helloworldService;
+    }
+
+    @ResponseBody
     @GetMapping
     public String index() {
-        return "helloworld!";
+        return helloworldService.getString();
     }
 }
---
+++ src/main/java/com/example/helloworld/helloworld/HelloworldService.java
@@ -0,0 +1,11 @@
+package com.example.helloworld.helloworld;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloworldService {
+
+    public String getString() {
+        return "helloworld";
+    }
+}
